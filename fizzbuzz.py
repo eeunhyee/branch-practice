9cdd4ec50eba8231ed2b38bb553cf9e89642ec0a
eeunhyee <eunhyee657@gmail.com>
2025-08-22 12:23:47 +0900

    feat: Add FizzBuzz Output For Multiples Of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
-for j in range(1, 15+1):
+for j in range(1, 45+1):
     if j % 3 == 0:
+        if j % 5 == 0:
+            print('fizzbuzz')
         print('fizz')
     elif j % 5 == 0:
+        if j % 3 == 0:
+            print('fizzbuzz')
         print('buzz')
     else:
         print(j)
